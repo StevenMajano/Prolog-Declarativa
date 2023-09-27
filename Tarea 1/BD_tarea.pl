4d1e56d649ff0b5e8314018c7c3259bbb582f56a
Stevenmajano <00064420@uca.edu.sv>
2023-09-27 15:46:55 -0600

    added .pl archive

---
+++ Tarea 1/BD_tarea.pl
@@ -0,0 +1,75 @@
+%base de conocimiento
+%lugares parques 
+lugares(parque_daniel_hernandez).
+lugares(parque_san_martin).
+lugares(parque_deportivo_el_cafetalon).
+lugares(parque_de_la_familia).
+lugares(parque_residencial_altos_de_santa_monica).
+lugares(club_tecleño).
+lugares(zona_verde_colonia_las_palmeras).
+lugares(parque_de_finca_de_asturias).
+%lugares centros de estudio
+lugares(colegio_santa_cecilia).
+lugares(colegio_champagnat).
+lugares(itca).
+lugares(liceo_frances).
+lugares(fesa).
+%lugares centros de salud 
+lugares(clinica_inmaculada_concepcion).
+lugares(hospital_climosal).
+lugares(hospital_san_rafael).
+%lugares gasolineras
+lugares(uno_delicias).
+lugares(texaco_delicias).
+%lugares centros cormerciales 
+lugares(centro_comercial_las_palmeras).
+lugares(plaza_merliot).
+lugares(centro_comercial_santa_rosa).
+lugares(centro_comercial_skina).
+lugares(centro_comercial_las_ramblas).
+%lugares restaurantes
+lugares(cerveceria_chatos).
+lugares(peche_cosme).
+lugares(pan_lilian).
+lugares(tipicos_margoth_skina).
+lugares(wendys_chiltupan).
+lugares(tipicos_margoth_santatecla).
+lugares(dragon_dorado).
+lugares(casa_parrillada).
+lugares(charlie_boy_pizza).
+lugares(la_herradura).
+lugares(mc_donalds_britanica).
+lugares(pasteleria_florece).
+%lugares supermercados
+lugares(despensa_de_don_juan).
+lugares(mercado_dueñas).
+lugares(el_baratillo).
+lugares(super_tienda_morena).
+lugares(coplasa).
+lugares(carniceria_el_angel).
+%lugares varios
+lugares(gimnasio_adolfo_pineda).
+lugares(alcaldia_municipal).
+lugares(phonecloud_sv).
+lugares(lubrimotors_repuestos).
+lugares(hogar_de_niños_adalberto_guirola).
+lugares(tecno_shop_sv).
+lugares(mi_casa).
+lugares(merys_beauty_salon).
+lugares(los_farolitos).
+lugares(co_industrias_gigantes).
+lugares(consejo_salvadoreño_del_cafe).
+lugares(ministerio_agricultura_ganaderia).
+lugares(casa_rivas).
+lugares(fisioterapia).
+lugares(residencial_finca_de_asturias).
+lugares(iglesia_del_camino).
+lugares(tienda_dinora).
+lugares(gtr_auto_parts).
+lugares(telus).
+lugares(kinetika).
+lugares(cementerio).
+lugares(sisa_seguros).
+lugares(banco_cuscatlan).
+lugares(grupo_lexa_sistemas_para_restaurantes).
+
